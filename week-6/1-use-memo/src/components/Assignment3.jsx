ff1c1dbe31d155e2ccb8be179d412337171ee8b5
NIGHTMARE09 <shivamjha,1299@gmail.com>
2025-04-26 02:38:32 +0530

    Complete week-6 React hooks assignments for useMemo, useCallback and useRef

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,17 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'aalu', value: 30 },
+        { name: 'ginger', value: 50},
+        { name: 'grapes', value: 30 },
+        { name: 'gobhi', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() =>{
+        return items.reduce((acc, item) => acc + item.value, 0);
+    }, [items]);
     // Your code ends here
     return (
         <div>
